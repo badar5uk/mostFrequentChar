e005fdce36aadca55cbb6acbcc8cb6be402a5cde
bader <bader5uk@hotmail.com>
2024-09-24 20:35:24 +0400

    removed user input for character

--- mostFrequentChar.java
+++ mostFrequentChar.java
@@ -7,15 +7,20 @@ public class mostFrequentChar {
         System.out.println("Enter a word: ");
         String word = userInp.next();
         char[] usertoCharArray = word.toCharArray();
+        int maxCount = 0;
+        char mostFrequentChar = word.charAt(0);
         int frequency = 0;
         for (int i = 0; i < word.length(); i++) {
             for (int j = i + 1; j < word.length(); j++) {
                 if (usertoCharArray[i] == usertoCharArray[j]) {
-                    System.out.println("The most frequent character is: "+ usertoCharArray[j]);
-                    frequency++;
-                    break;
+                    ++frequency;
+                }
+                if (frequency > maxCount) {
+                    maxCount = frequency;
+                    mostFrequentChar = usertoCharArray[i];
                 }
             }
         }
+        System.out.println("The most frequent character is: " + mostFrequentChar);
     }
 }
